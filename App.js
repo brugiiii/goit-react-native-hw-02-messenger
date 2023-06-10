1fed4555d8bb46726471f6f86ef61f4b6599fda3
Eduard <edyabrug91@gmail.com>
2023-06-10 20:14:58 +0300

    done

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { PostsScreen, RegistrationScreen, LoginScreen } from "./src/screens";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isLogIn, setIsLogIn] = useState(false);
+  const [switcher, setSwitcher] = useState(false);
+
+  const toggle = () => {
+    setSwitcher(!switcher);
+  };
+
+  return isLogIn ? (
+    <PostsScreen />
+  ) : switcher ? (
+    <RegistrationScreen toggle={toggle} />
+  ) : (
+    <LoginScreen toggle={toggle} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
